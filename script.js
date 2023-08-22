a5f299fcd4604d0a2befe90f1c680c3bd10c091f
accio-f3 <acciof3@gmail.com>
2023-08-22 21:27:04 +0530

    google excel clone - 3

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-// excel dimensions
+// excel dimensions and constants
 const rows=100;
 const columns = 26;
+const transparentBlue='#ddddff';
+const transparent='transparent';
 
-// elements
+// excel components
 const tHeadRow = document.getElementById('table-heading-row');
 const tBody = document.getElementById('table-body');
-const boldBtn = document.getElementById('bold-btn');
 const currentCellHeading=document.getElementById('current-cell');
+
+// excel Buttons
+const boldBtn = document.getElementById('bold-btn');
+const italicsBtn = document.getElementById('italics-btn');
+const underlineBtn = document.getElementById('underline-btn');
+const leftBtn = document.getElementById('left-btn');
+const centerBtn = document.getElementById('center-btn');
+const rightBtn = document.getElementById('right-btn');
+
+// excel dropdowns
+const fontDropdown = document.getElementById('fonte-style-dropdown');
 
 // variables to save cache
 let prevCellId;
@@ -37,21 +49,72 @@ function setCellColor(colId,rowId,color){
     rowHead.style.backgroundColor=color;
 }
 
+function buttonHighlter(currentCell, button, style, styleProperty) {
+    if(currentCell.style[styleProperty]===style){
+        button.style.backgroundColor=transparentBlue;
+    }
+    else{
+        button.style.backgroundColor=transparent;
+    }
+}
+
+// function buttonClickHandler(currentCell, button, style, toggleStyle, styleProperty) {
+//     button.addEventListener('click',()=>{
+//         if(currentCell.style[styleProperty]===style){
+//             currentCell.style[styleProperty]=toggleStyle;
+//             button.style.backgroundColor=transparent;
+//         }
+//         else{
+//             currentCell.style[styleProperty]=style;
+//             button.style.backgroundColor=transparentBlue;
+//         }
+//     })
+// }
+
+// buttonClickHandler(currentCell,boldBtn,'bold','normal','fontWeight');
+
 function onFocusFunction(cell){
     currentCell=cell;
+    // 
     if(prevCellId){
         // const colHead = document.getElementById(prevCellId[0]);
         // const rowHead= document.getElementById(prevCellId.substring(1));
-        // colHead.style.backgroundColor='transparent';
-        // rowHead.style.backgroundColor='transparent';
-        setCellColor(prevCellId[0],prevCellId.substring(1),'transparent');
+        // colHead.style.backgroundColor=transparent;
+        // rowHead.style.backgroundColor=transparent;
+        setCellColor(prevCellId[0],prevCellId.substring(1),transparent);
     }
+    // 
+    // if(currentCell.style.fontWeight==='bold'){
+    //     boldBtn.style.backgroundColor=transparentBlue;
+    // }
+    // else{
+    //     boldBtn.style.backgroundColor=transparent;
+    // }
+    buttonHighlter(currentCell,boldBtn,'bold','fontWeight');
+    // 
+    // if(currentCell.style.fontStyle==='italic'){
+    //     italicsBtn.style.backgroundColor=transparentBlue;
+    // }
+    // else{
+    //     italicsBtn.style.backgroundColor=transparent;
+    // }
+    buttonHighlter(currentCell,italicsBtn,'italic','fontStyle');
+    // 
+    // if(currentCell.style.textDecoration==='underline'){
+    //     underlineBtn.style.backgroundColor=transparentBlue;
+    // }
+    // else{
+    //     underlineBtn.style.backgroundColor=transparent;
+    // }
+    // 
+    buttonHighlter(currentCell,underlineBtn,'underline','textDecoration');
+    // 
     currentCellHeading.innerText=cell.id + ' ' + 'selected';
     // const colHead = document.getElementById(cellId[0]);
     // const rowHead=document.getElementById(cellId.substring(1));
-    // colHead.style.backgroundColor='#ddddff';
-    // rowHead.style.backgroundColor='#ddddff';
-    setCellColor(cell.id[0],cell.id.substring(1),'#ddddff');
+    // colHead.style.backgroundColor=transparentBlue;
+    // rowHead.style.backgroundColor=transparentBlue;
+    setCellColor(cell.id[0],cell.id.substring(1),transparentBlue);
     // here my cellId becomes prev cell id
     prevCellId=cell.id;
 }
@@ -82,14 +145,52 @@ for (let row = 1; row <= rows; row++) {
 }
 
 boldBtn.addEventListener('click',()=>{
-    console.log(currentCell.style.fontWeight);
     if(currentCell.style.fontWeight==='bold'){
         currentCell.style.fontWeight='normal';
+        boldBtn.style.backgroundColor=transparent;
     }
     else{
         currentCell.style.fontWeight='bold';
+        boldBtn.style.backgroundColor=transparentBlue;
     }
 })
-// Q?
-// Bold Button should show active state for bolded cell and non active
-// for non bolded cell
+
+italicsBtn.addEventListener('click',()=>{
+    if(currentCell.style.fontStyle==='italic'){
+        currentCell.style.fontStyle='normal';
+        italicsBtn.style.backgroundColor=transparent;
+    }
+    else{
+        currentCell.style.fontStyle='italic';
+        italicsBtn.style.backgroundColor=transparentBlue;
+    }
+})
+// buttonClickHandler(currentCell,boldBtn,'bold','normal','fontWeight');
+
+underlineBtn.addEventListener('click',()=>{
+    if(currentCell.style.textDecoration==='underline'){
+        currentCell.style.textDecoration='none';
+        underlineBtn.style.backgroundColor=transparent;
+    }
+    else{
+        currentCell.style.textDecoration='underline';
+        underlineBtn.style.backgroundColor=transparentBlue;
+    }
+})
+// buttonClickHandler(currentCell,boldBtn,'bold','normal','fontWeight');
+
+leftBtn.addEventListener('click',()=>{
+    currentCell.style.textAlign='left';
+})
+
+rightBtn.addEventListener('click',()=>{
+    currentCell.style.textAlign='right';
+})
+
+centerBtn.addEventListener('click',()=>{
+    currentCell.style.textAlign='center';
+})
+
+fontDropdown.addEventListener('change',()=>{
+    currentCell.style.fontFamily=fontDropdown.value;
+})
